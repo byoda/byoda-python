02e4e03e592d71d3c2dc7f017846a388125beb82
Steven Hessing <stevenhessing@live.com>
2022-05-26 21:49:20 -0700

    put graphql query strings in a separate test module

--- tests/func/pod_apis.py
+++ tests/func/pod_apis.py
@@ -47,6 +47,14 @@ from podserver.routers import authtoken
 
 from tests.lib import get_test_uuid
 
+from tests.lib.graphql_queries import QUERY_PERSON
+from tests.lib.graphql_queries import MUTATE_PERSON
+from tests.lib.graphql_queries import QUERY_NETWORK
+from tests.lib.graphql_queries import QUERY_NETWORK_WITH_FILTER
+from tests.lib.graphql_queries import APPEND_NETWORK
+from tests.lib.graphql_queries import UPDATE_NETWORK_RELATION
+from tests.lib.graphql_queries import DELETE_FROM_NETWORK_WITH_FILTER
+
 # Settings must match config.yml used by directory server
 NETWORK = config.DEFAULT_NETWORK
 
@@ -61,92 +69,6 @@ ADDRESSBOOK_SERVICE_ID = None
 ADDRESSBOOK_VERSION = 1
 
 EVENTS = []
-
-PERSON_QUERY = '''
-query {
-    person {
-        given_name
-        additional_names
-        family_name
-        email
-        homepage_url
-        avatar_url
-    }
-}
-'''
-
-MUTATE_PERSON = '''
-mutation {{
-    mutate_person(
-        given_name: "{given_name}",
-        additional_names: "",
-        family_name: "{family_name}",
-        email: "{email}",
-        homepage_url: "https://some.place/",
-        avatar_url: "https://some.place/avatar"
-    ) {{
-        given_name
-        additional_names
-        family_name
-        email
-        homepage_url
-        avatar_url
-    }}
-}}
-'''
-
-QUERY_NETWORK = '''
-query {
-    network_links {
-        relation
-        member_id
-        timestamp
-    }
-}
-'''
-
-QUERY_NETWORK_WITH_FILTER = '''
-query {{
-    network_links(filters: {{ {field}: {{ {cmp}: "{value}" }} }}) {{
-        relation
-        member_id
-        timestamp
-    }}
-}}
-'''
-
-APPEND_NETWORK = '''
-mutation {{
-    append_network_links (
-        member_id: "{uuid}",
-        relation: "{relation}",
-        timestamp: "{timestamp}"
-    ) {{
-        member_id relation timestamp
-    }}
-}}
-'''
-
-UPDATE_NETWORK_RELATION = '''
-mutation {{
-    update_network_links (
-        filters: {{ {field}: {{ {cmp}: "{value}" }} }},
-        relation: "{relation}",
-    ) {{
-        member_id relation timestamp
-    }}
-}}
-'''
-
-DELETE_FROM_NETWORK_WITH_FILTER = '''
-mutation {{
-    delete_from_network_links(filters: {{ {field}: {{ {cmp}: "{value}" }} }}) {{
-        relation
-        member_id
-        timestamp
-    }}
-}}
-'''
 
 
 class TestDirectoryApis(unittest.IsolatedAsyncioTestCase):
@@ -282,7 +204,7 @@ class TestDirectoryApis(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(data['loglevel'], 'DEBUG')
         self.assertEqual(data['private_key_secret'], 'byoda')
         self.assertEqual(data['bootstrap'], True)
-        self.assertEqual(len(data['services']), 2)
+        self.assertEqual(len(data['services']), 1)
 
         # Get the service ID for the addressbook service
         service_id = None
@@ -388,7 +310,7 @@ class TestDirectoryApis(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(data['loglevel'], 'DEBUG')
         self.assertEqual(data['private_key_secret'], 'byoda')
         self.assertEqual(data['bootstrap'], True)
-        self.assertEqual(len(data['services']), 2)
+        self.assertEqual(len(data['services']), 1)
 
         API = BASE_URL + '/v1/pod/member'
         response = requests.get(
@@ -563,7 +485,7 @@ class TestDirectoryApis(unittest.IsolatedAsyncioTestCase):
             result['data']['mutate_person']['given_name'], 'Carl'
         )
 
-        result = client.execute(query=PERSON_QUERY, headers=member_headers)
+        result = client.execute(query=QUERY_PERSON, headers=member_headers)
 
         result = client.execute(
             query=MUTATE_PERSON.format(
@@ -602,7 +524,7 @@ class TestDirectoryApis(unittest.IsolatedAsyncioTestCase):
         }
 
         # Query fails because other members do not have access
-        result = client.execute(PERSON_QUERY, headers=alt_member_headers)
+        result = client.execute(QUERY_PERSON, headers=alt_member_headers)
         self.assertIsNone(result['data'])
         self.assertIsNotNone(result['errors'])
 
