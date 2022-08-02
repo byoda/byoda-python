77e68ad46493b119baa0df2428cd0980274ea2c3
Steven Hessing <stevenhessing@live.com>
2022-08-01 22:27:25 -0700

    persistence of newest pod in podworker.py now works

--- podserver/podworker.py
+++ podserver/podworker.py
@@ -44,8 +44,6 @@ from byoda.servers.pod_server import PodServer
 from byoda import config
 from byoda.util.logger import LOGFILE, Logger
 
-from byoda.exceptions import PodException
-
 from podserver.util import get_environment_vars
 
 from byoda.data_import.twitter import Twitter
@@ -59,7 +57,7 @@ from tests.lib.addressbook_queries import APPEND_TWITTER_MEDIAS
 _LOGGER = None
 LOG_FILE = '/var/www/wwwroot/logs/podworker.log'
 ADDRESSBOOK_ID = 4294929430
-
+NEWEST_TWEET_FILE = 'newest_tweet.txt'
 
 async def main(argv):
     # Remaining environment variables used:
@@ -234,42 +232,80 @@ def twitter_update_task():
         raise
 
 
-def fetch_tweets(twitter_client: Twitter):
-    _LOGGER.debug('Fetching tweets')
-    account: Account = config.server.account
+def determine_newest_tweet(account: Account, member: Member,
+                           graphql_url: str) -> str:
+    '''
+    This function first looks for a local file under /byoda to see if
+    the ID of the newest tweet is stored there. If it is, it returns
+    that, otherwise it gets all tweets from the pod to see what the
+    newest is
+
+    :returns: string with the integer value of the newest Tweet in the pod
+    '''
+
+    _LOGGER.debug('Figuring out newest tweet in the pod')
+
     local_path: str = account.document_store.backend.local_path
-    newest_tweet_file = local_path + 'newest_tweet.txt'
+
+    newest_tweet_file = local_path + NEWEST_TWEET_FILE
+
     try:
         with open(newest_tweet_file, 'r') as file_desc:
             newest_tweet = file_desc.read().strip()
-            _LOGGER.debug(
-                f'Read newest tweet_id {newest_tweet} from {newest_tweet_file}'
-            )
+        _LOGGER.debug(
+            f'Read newest tweet_id {newest_tweet} from {newest_tweet_file}'
+        )
+        return newest_tweet
     except OSError:
         newest_tweet = None
 
-    member: Member = config.server.account.memberships.get(ADDRESSBOOK_ID)
-    url = f'https://{member.tls_secret.common_name}'
-    url += GRAPHQL_API_URL_PREFIX.format(service_id=member.service_id)
-
-    if not newest_tweet:
-        _LOGGER.debug(f'Newest tweet not read from {newest_tweet_file}')
-        resp = GraphQlClient.call_sync(
-            url, QUERY_TWEETS, secret=member.tls_secret
+    _LOGGER.debug(f'Newest tweet not read from {newest_tweet_file}')
+    resp = GraphQlClient.call_sync(
+        graphql_url, QUERY_TWEETS, secret=member.tls_secret
+    )
+    data = resp.json()
+    edges = data['data']['tweets_connection']['edges']
+    if len(edges):
+        _LOGGER.debug(
+            f'Discovering newest tweet ID from {len(edges)} tweets from '
+            'the pod'
         )
-        data = resp.json()
-        edges = data['data']['tweets_connection']['edges']
-        if len(edges):
-            _LOGGER.debug(
-                f'Discovering newest tweet ID from {len(edges)} tweets from '
-                'the pod'
-            )
-            tweet_ids = set([edge['tweet']['asset_id'] for edge in edges])
-            sorted_tweet_ids = sorted(tweet_ids, reverse=True)
-            newest_tweet = sorted_tweet_ids[0]
-            _LOGGER.debug(f'Newest tweet ID in the pod is {newest_tweet}')
-        else:
-            _LOGGER.debug('No tweets found in the pod')
+        tweet_ids = set([edge['tweet']['asset_id'] for edge in edges])
+        sorted_tweet_ids = sorted(tweet_ids, reverse=True)
+        newest_tweet = sorted_tweet_ids[0]
+        _LOGGER.debug(f'Newest tweet ID in the pod is {newest_tweet}')
+    else:
+        _LOGGER.debug('No tweets found in the pod')
+
+    return newest_tweet
+
+
+def persist_newest_tweet(newest_tweet_file, newest_tweet) -> None:
+    '''
+    Persists the asset ID of the newest tweet in the pod to local storage
+    '''
+
+    try:
+        with open(newest_tweet_file, 'w') as file_desc:
+            file_desc.write(newest_tweet)
+        _LOGGER.debug(
+            f'Write newest tweet_id {newest_tweet} '
+            f'to {newest_tweet_file}'
+        )
+    except OSError:
+        pass
+
+
+def fetch_tweets(twitter_client: Twitter):
+    _LOGGER.debug('Fetching tweets')
+
+    account: Account = config.server.account
+    member: Member = config.server.account.memberships.get(ADDRESSBOOK_ID)
+
+    graphql_url = f'https://{member.tls_secret.common_name}'
+    graphql_url += GRAPHQL_API_URL_PREFIX.format(service_id=member.service_id)
+
+    newest_tweet = determine_newest_tweet(account, member, graphql_url)
 
     all_tweets, referencing_tweets, media = \
         twitter_client.get_tweets(since_id=newest_tweet, with_related=True)
@@ -278,7 +314,8 @@ def fetch_tweets(twitter_client: Twitter):
         _LOGGER.debug(f'Processing tweet {tweet["asset_id"]}')
         try:
             resp: requests.Response = GraphQlClient.call_sync(
-                url, APPEND_TWEETS, vars=tweet, secret=member.tls_secret
+                graphql_url, APPEND_TWEETS, vars=tweet,
+                secret=member.tls_secret
             )
         except Exception as exc:
             _LOGGER.info(
@@ -308,25 +345,15 @@ def fetch_tweets(twitter_client: Twitter):
     # the ID of the newest tweet
     if len(all_tweets):
         newest_tweet = all_tweets[0]['asset_id']
+        persist_newest_tweet(newest_tweet, newest_tweet)
     else:
         _LOGGER.debug('There were no newer tweets available from Twitter')
-
-    if newest_tweet:
-        try:
-            with open(newest_tweet_file, 'w') as file_desc:
-                file_desc.write(newest_tweet)
-                _LOGGER.debug(
-                    f'Write newest tweet_id {newest_tweet} '
-                    f'to {newest_tweet_file}'
-                )
-        except OSError:
-            pass
 
     for asset in media:
         _LOGGER.debug(f'Processing Twitter media ID {asset["media_key"]}')
         try:
             resp: requests.Response = GraphQlClient.call_sync(
-                url, APPEND_TWITTER_MEDIAS, vars=asset,
+                graphql_url, APPEND_TWITTER_MEDIAS, vars=asset,
                 secret=member.tls_secret
             )
         except Exception as exc:
