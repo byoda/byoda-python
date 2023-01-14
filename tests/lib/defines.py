1125a7dccb2eaf94eca094af22adeb0948c53268
Steven Hessing <stevenhessing@live.com>
2023-01-13 19:03:15 -0800

    add test pod IDs

--- tests/lib/defines.py
+++ tests/lib/defines.py
@@ -6,21 +6,26 @@ Static variable definitions used in various test cases
 :license
 '''
 
-AZURE_POD_ACCOUNT_ID = '7ea17e0e-19ae-4911-ab83-a165213810c4'
-AZURE_POD_MEMBER_ID = 'daf2148e-0e52-42b0-a4fa-05a07cf8d7ba'
-AZURE_POD_SECRET_FILE = \
-    'tests/collateral/local/azure-pod-account-secret.passwd'
-
 BASE_URL = 'http://localhost:{PORT}/api'
 
 ADDRESSBOOK_SERVICE_ID = 4294929430
 ADDRESSBOOK_VERSION = 1
 
+
+AZURE_POD_ACCOUNT_ID = '7ea17e0e-19ae-4911-ab83-a165213810c4'
+AZURE_POD_MEMBER_ID = 'daf2148e-0e52-42b0-a4fa-05a07cf8d7ba'
+AZURE_POD_CUSTOM_DOMAIN = 'azure.byoda.me'
+AZURE_POD_SECRET_FILE = \
+    'tests/collateral/local/azure-pod-account-secret.passwd'
+
 GCP_POD_ACCOUNT_ID = '10703b80-967d-4b80-912f-d943b76912ed'
 GCP_POD_MEMBER_ID = 'f1d84ed6-9e52-489a-a9f3-57b73aa3e3e1'
+GCP_POD_CUSTOM_DOMAIN = 'gcp.byoda.me'
 
 AWS_POD_ACCOUNT_ID = '71776c5d-872d-4569-9d77-b9dca201f916'
 AWS_POD_MEMBER_ID = 'c00da08d-f95a-473f-92dd-3b8f6c3143bc'
+AWS_POD_CUSTOM_DOMAIN = 'aws.byoda.me'
 
 HOME_POD_ACCOUNT_ID = '10948fcf-7270-4c13-bfa6-016e7df8d49b'
 HOME_POD_MEMBER_ID = '183e073e-28e6-4acf-8063-bcada692767d'
+HOME_POD_CUSTOM_DOMAIN = ''
