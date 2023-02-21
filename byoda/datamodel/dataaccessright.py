79b867938aea4c7033370776ed67963a7447e6f3
Steven Hessing <stevenhessing@live.com>
2023-02-20 22:26:59 -0800

    fix iterating over network_links for access rights when no network links were found

--- byoda/datamodel/dataaccessright.py
+++ byoda/datamodel/dataaccessright.py
@@ -245,7 +245,7 @@ class NetworkDataAccessRight(DataAccessRight):
                 f'Found total of {len(network_links or [])} network links'
             )
             network = [
-                link for link in network_links
+                link for link in network_links or []
                 if (link['member_id'] == auth.member_id
                     and (not self.relations or
                     link['relation'].lower() in self.relations))
